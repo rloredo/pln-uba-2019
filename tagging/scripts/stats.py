a29b3384d84f13df7e8ab99daee8f14123b02dcd
rori <loredo.rod@gmail.com>
2019-09-14 12:33:47 -0300

    Resolucion stats

--- tagging/scripts/stats.py
+++ tagging/scripts/stats.py
@@ -19,64 +19,104 @@ class POSStats:
 
     def __init__(self, tagged_sents):
         """
-        tagged_sents -- corpus (list/iterable/generator of tagged sentences)
+        tagged_sents 
+        -- corpus (list/iterable/generator of tagged sentences)
         """
-        # WORK HERE!!
-        # COLLECT REQUIRED STATISTICS INTO DICTIONARIES.
-
+        #List of tagged sentences
+        self.corpus = list(tagged_sents) #Load lazyMap into memory... Takes time but I don't know very well how to work with this ADT
+        
+        #Create set of words and tags
+        #This could be moved and only used in the functions that requires it
+        #but since different functions uses it, I chose to leave it here and load it once 
+        
+        #Flatten the corpus
+        self.flat_corpus = [tupla for sentence in self.corpus for tupla in sentence]
+        #Then get words and tags
+        self.corpus_words, self.corpus_tags = zip(*self.flat_corpus)   
+        
+        #
+        #
+        #Create dictionaries for other stuff
+        #Create a word dict (keys are words and values are POS tags + n of occurencies)
+        self.word_dict = defaultdict(lambda: defaultdict(int))
+        
+        for sent in self.corpus:
+                for word, tag in sent:
+                    # Save the word and tags:
+                    self.word_dict[word][tag] += 1
+        self.word_dict = dict(self.word_dict)
+        
+        #We create a tag dict (keys are tags and values are words with those tags + n of occurencies)
+        self.tag_dict = defaultdict(lambda: defaultdict(int))
+        
+        for sent in self.corpus:
+                for word, tag in sent:
+                    # Save the tag and words:
+                    self.tag_dict[tag][word] += 1
+        self.tag_dict = dict(self.tag_dict)        
+      
+        
     def sent_count(self):
         """Total number of sentences."""
-        # WORK HERE!!
+        return len(self.corpus)
 
     def token_count(self):
         """Total number of tokens."""
-        # WORK HERE!!
-
+        return len(self.corpus_words)
+                
     def words(self):
         """Vocabulary (set of word types)."""
-        # WORK HERE!!
+#       return list(set(self.corpus_words))
+        return list(self.word_dict.keys())
 
     def word_count(self):
         """Vocabulary size."""
-        # WORK HERE!!
+        return len(self.words())
 
     def word_freq(self, w):
-        """Frequency of word w."""
-        # WORK HERE!!
+        """Frequency of word w."""                    
+        return self.corpus_words.count(w) / self.word_count()
 
     def unambiguous_words(self):
         """List of words with only one observed POS tag."""
-        # WORK HERE!!
+        return [key for key in self.word_dict.keys() if len(self.word_dict[key].keys()) == 1]
 
     def ambiguous_words(self, n):
         """List of words with n different observed POS tags.
 
         n -- number of tags.
-        """
-        # WORK HERE!!
-
+        """      
+        return [key for key in self.word_dict.keys() if len(self.word_dict[key].keys()) == n]
+              
     def tags(self):
         """POS Tagset."""
-        # WORK HERE!!
+#       return list(set(self.corpus_tags))
+        return list(self.tag_dict.keys())
 
     def tag_count(self):
         """POS tagset size."""
-        # WORK HERE!!
+        return len(self.tags())
 
     def tag_freq(self, t):
-        """Frequency of tag t."""
-        # WORK HERE!!
+        """Frequency of tag t."""                   
+        return self.corpus_tags.count(t) / self.tag_count()  
 
     def tag_word_dict(self, t):
         """Dictionary of words and their counts for tag t."""
+            
         return dict(self._tcount[t])
 
 
+
+
+
+
+#Esto corre el programa
 if __name__ == '__main__':
     opts = docopt(__doc__)
 
     # load the data
-    corpus = SimpleAncoraCorpusReader(opts['-c'])
+    corpus = SimpleAncoraCorpusReader(opts['<path>']) # opts -c returns boolean, path returns path
     sents = corpus.tagged_sents()
 
     # compute the statistics
@@ -91,24 +131,33 @@ if __name__ == '__main__':
     print('words: {}'.format(word_count))
     print('tags: {}'.format(stats.tag_count()))
     print('')
-
+    print('Example of word frequency')
+    print('================')
+    print('Frequency of "presidente": {}'.format(stats.word_freq('presidente')))
+    print('')
+    print('Example of tag frequency')
+    print('================')
+    print('Frequency of "nc0s000": {}'.format(stats.tag_freq('nc0s000')))
+    print('')
     print('Most Frequent POS Tags')
     print('======================')
     tags = [(t, stats.tag_freq(t)) for t in stats.tags()]
     sorted_tags = sorted(tags, key=lambda t_f: -t_f[1])
-    print('tag\tfreq\t%\ttop')
-    for t, f in sorted_tags[:10]:
-        words = stats.tag_word_dict(t).items()
-        sorted_words = sorted(words, key=lambda w_f: -w_f[1])
-        top = [w for w, _ in sorted_words[:5]]
-        print('{0}\t{1}\t{2:2.2f}\t({3})'.format(t, f, f * 100 / token_count, ', '.join(top)))
+    print(sorted_tags[:10])
+    
+#    print('tag\tfreq\t%\ttop')
+#    for t, f in sorted_tags[:10]:
+#        words = stats.tag_word_dict(t).items()
+#        sorted_words = sorted(words, key=lambda w_f: -w_f[1])
+#        top = [w for w, _ in sorted_words[:5]]
+#        print('{0}\t{1}\t{2:2.2f}\t({3})'.format(t, f, f * 100 / token_count, ', '.join(top)))
+    
     print('')
-
     print('Word Ambiguity Levels')
-    print('=====================')
+    print('=====================')    
     print('n\twords\t%\ttop')
     for n in range(1, 10):
-        words = list(stats.ambiguous_words(n))
+        words = stats.ambiguous_words(n)
         m = len(words)
 
         # most frequent words:
