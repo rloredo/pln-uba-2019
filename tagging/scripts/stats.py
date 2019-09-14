834521a8283d092a26d6b3b1cdc09e025fc83953
rori <loredo.rod@gmail.com>
2019-09-14 16:17:34 -0300

    completado tagwordict. refactorizacion. Cambio en computo frecuencias (x/total a x)

--- tagging/scripts/stats.py
+++ tagging/scripts/stats.py
@@ -24,38 +24,35 @@ class POSStats:
         """
         #List of tagged sentences
         self.corpus = list(tagged_sents) #Load lazyMap into memory... Takes time but I don't know very well how to work with this ADT
-        
-        #Create set of words and tags
-        #This could be moved and only used in the functions that requires it
-        #but since different functions uses it, I chose to leave it here and load it once 
-        
-        #Flatten the corpus
-        self.flat_corpus = [tupla for sentence in self.corpus for tupla in sentence]
-        #Then get words and tags
-        self.corpus_words, self.corpus_tags = zip(*self.flat_corpus)   
-        
+              
         #
         #
-        #Create dictionaries 
         #Create a word dict (keys are words and values are POS tags + n of occurencies)
         self.word_dict = defaultdict(lambda: defaultdict(int))
+        #Create a tag dict (keys are tags and values are words + n of occurencies)
+        self.tag_dict = defaultdict(lambda: defaultdict(int))
         #Create a tag dict (keys are tags and values n of occurencies)
-        self.tag_dict = defaultdict(int)
+        self.freq_tag_dict = defaultdict(int)
         #Create a freq dict (keys are words and values are n of occurencies)
-        self.freq_dict = defaultdict(int)
+        self.freq_word_dict = defaultdict(int)
+        #Create token counter
+        self.tokenCount = 0
         
         for sent in self.corpus:
                 for word, tag in sent:
                    
                     self.word_dict[word][tag] += 1
-                    self.tag_dict[tag] += 1
-                    self.freq_dict[word] += 1
+                    self.tag_dict[tag][word] += 1
+                    self.freq_tag_dict[tag] += 1
+                    self.freq_word_dict[word] += 1
+                    self.tokenCount += 1
                     
-                    
+        #Convert dicts back to normal dicts            
         self.word_dict = dict(self.word_dict)                   
         self.tag_dict = dict(self.tag_dict)        
-        self.freq_dict = dict(self.freq_dict) 
-      
+        self.freq_word_dict = dict(self.freq_word_dict) 
+        self.freq_tag_dict = dict(self.freq_tag_dict) 
+   
         
     def sent_count(self):
         """Total number of sentences."""
@@ -63,12 +60,11 @@ class POSStats:
 
     def token_count(self):
         """Total number of tokens."""
-        return len(self.corpus_words)
+        return self.tokenCount
                 
     def words(self):
         """Vocabulary (set of word types)."""
-#       return list(set(self.corpus_words))
-        return list(self.word_dict.keys())
+        return list(self.freq_word_dict.keys())
 
     def word_count(self):
         """Vocabulary size."""
@@ -76,7 +72,8 @@ class POSStats:
 
     def word_freq(self, w):
         """Frequency of word w."""                    
-        return self.freq_dict[w] / self.word_count()
+#        return self.freq_dict[w] / self.tokenCount
+        return self.freq_word_dict[w]
 
     def unambiguous_words(self):
         """List of words with only one observed POS tag."""
@@ -91,8 +88,7 @@ class POSStats:
               
     def tags(self):
         """POS Tagset."""
-#       return list(set(self.corpus_tags))
-        return list(self.tag_dict.keys())
+        return list(self.freq_tag_dict.keys())
 
     def tag_count(self):
         """POS tagset size."""
@@ -100,17 +96,12 @@ class POSStats:
 
     def tag_freq(self, t):
         """Frequency of tag t."""                   
-        return self.corpus_tags.count(t) / self.tag_count()  
+#        return self.tag_dict[t] / self.tokenCount  
+        return self.freq_tag_dict[t]
 
     def tag_word_dict(self, t):
-        """Dictionary of words and their counts for tag t."""
-            
-        return dict(self._tcount[t])
-
-
-
-
-
+        """Dictionary of words and their counts for tag t."""         
+        return dict(self.tag_dict[t])
 
 #Esto corre el programa
 if __name__ == '__main__':
@@ -144,15 +135,12 @@ if __name__ == '__main__':
     print('======================')
     tags = [(t, stats.tag_freq(t)) for t in stats.tags()]
     sorted_tags = sorted(tags, key=lambda t_f: -t_f[1])
-    print(sorted_tags[:10])
-    
-#    print('tag\tfreq\t%\ttop')
-#    for t, f in sorted_tags[:10]:
-#        words = stats.tag_word_dict(t).items()
-#        sorted_words = sorted(words, key=lambda w_f: -w_f[1])
-#        top = [w for w, _ in sorted_words[:5]]
-#        print('{0}\t{1}\t{2:2.2f}\t({3})'.format(t, f, f * 100 / token_count, ', '.join(top)))
-    
+    print('tag\tfreq\t%\ttop')
+    for t, f in sorted_tags[:10]:
+        words = stats.tag_word_dict(t).items()
+        sorted_words = sorted(words, key=lambda w_f: -w_f[1])
+        top = [w for w, _ in sorted_words[:5]]
+        print('{0}\t{1}\t{2:2.2f}\t({3})'.format(t, f, f * 100 / token_count, ', '.join(top)))
     print('')
     print('Word Ambiguity Levels')
     print('=====================')    
