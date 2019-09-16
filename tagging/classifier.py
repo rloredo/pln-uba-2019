71007a0bedbfb12d52630bd854d9841117d339f3
rori <loredo.rod@gmail.com>
2019-09-16 10:34:35 -0300

    Arreglado extraccion de features

--- tagging/classifier.py
+++ tagging/classifier.py
@@ -1,52 +1,48 @@
 from sklearn.pipeline import Pipeline
 from sklearn.feature_extraction import DictVectorizer
 from sklearn.svm import LinearSVC
-from sklearn.linear_model import LogisticRegression
 from sklearn.naive_bayes import MultinomialNB
+from sklearn.linear_model import LogisticRegression
 from tqdm import tqdm
 
 classifiers = {
     'lr': LogisticRegression,
     'svm': LinearSVC,
-    'nbc': MultinomialNB,
+    'mnb': MultinomialNB,
 }
 
-
 def feature_dict(sent, i):
-    """Feature dictionary for a given sentence and position.
-
-    sent -- the sentence.
-    i -- the position.
-    """
-    # WORK HERE!!
+    feat_dict = {}
     
-    feature_dict = {}  #This is the dictionary that will be returned
-
-    #Features for all words
-    features = {'lw': str.lower, 'tw': str.istitle, 'uw': str.isupper, 'wd': str.isdigit}
+    features = {
+        "w": str.lower,
+        "wu": str.isupper,
+        "wt": str.istitle,
+        "wd": str.isdigit,
+    }
     
-    #Current word
-    words = {'current': sent[i][0]}
-    
-    #Previous word
+    #Current word, previous, next (add sentence start/finish as makers)
+    words = {'currentW': sent[i]}
     if i == 0:
-        words['previous'] = ' '
+        words['prevW'] = '<s>'
     else:
-        words['previous'] = sent[i-1][0]
+        words['prevW'] = sent[i - 1]
+    if i == len(sent) - 1:
+        words['nextW'] = '</s>'
+    else:
+        words['nextW'] = sent[i + 1]
     
-    #Next word
-    if i == len(sent)-1:
-        words['next'] = ' '
-    else:
-        words['next'] = sent[i+1][0]
-        
-    #Apply features. key: current/prev/next _feature  value: value
+    # Extract features
     for key, value in words.items():
-        for name, feature in features.items():
-            feature_dict[key.lstrip()+'_'+name] = feature(value)            
-    return feature_dict
-
-
+        if value == '<s>':
+            feat_dict['pw'] = value.lower()
+        elif value == '</s>':
+            feat_dict['nw'] = value.lower()
+        else:
+            for name, feature in features.items():
+                feat_dict[key.lstrip()+'_'+name] = feature(value)
+                
+    return feat_dict
 
 class ClassifierTagger:
     """Simple and fast classifier based tagger.
@@ -54,80 +50,62 @@ class ClassifierTagger:
 
     def __init__(self, tagged_sents, clf='lr'):
         """
-        clf -- classifying model, one of 'svm', 'nbc', 'lr' (default: 'lr').
+        clf -- classifying model, one of 'svm', 'lr' (default: 'lr').
         """
-        self.word_set = set()
-        
-        self.pipeline = Pipeline([
-            ('vect', DictVectorizer()),
-            ('clf', classifiers[clf]()),])
+        self.pipeline = Pipeline(
+                steps=[
+                    ('vect', DictVectorizer(sparse=True)),
+                    ('clf', classifiers[clf]())
+                 ])
 
         self.fit(tagged_sents)
 
     def fit(self, tagged_sents):
         """
         Train.
-
         tagged_sents -- list of sentences, each one being a list of pairs.
         """
+        print('Extracting features')
+        self.getXY(tagged_sents)
         print('')
-        print('extracting features')
-        X, y = self.trainSet(tagged_sents)
-        print('')
-        print('fitting')
-        self.pipeline.fit(X, y)
+        print('Executing pipeline')
+        self.pipeline.fit(self.X, self.y)
+
+    def getXY(self, tagged_sents):
+        X, y = [], []
+        words = set()     
+      
+        for tagged_sent in tqdm(tagged_sents):
+            
+            if not tagged_sents:
+                continue
+            if len(tagged_sent) == 0:
+                continue
+            
+            sent_words, sent_tags = zip(*tagged_sent)
+            y.extend(sent_tags)
+            words.update(sent_words)
+            
+            for i in range(len(sent_words)):    
+                X.append(feature_dict(sent_words, i))
+
+        self.X, self.y, self.words = X, y, words
 
     def tag_sents(self, sents):
         """Tag sentences.
-
         sent -- the sentences.
         """
         return [self.tag(sent) for sent in sents]
 
     def tag(self, sent):
         """Tag a sentence.
-
         sent -- the sentence.
         """
-        x = []
-        for i in range(len(sent)):
-            x.append(feature_dict(sent, i))
-            
-        tags = list(self.pipeline.predict(x))
-        return tags
+        return [self.pipeline.predict(
+                    [feature_dict(sent, i)for i in range(len(sent))] )][0]
 
     def unknown(self, w):
         """Check if a word is unknown for the model.
-
         w -- the word.
         """
-        
-        return not(w in self.word_set)
-        
-        
-    def trainSet(self, tagged_sent):
-        """
-            Gets the features (Xs) and correct tag (y)
-        
-        """
-        X = []
-        y = []
-        word_set = set()
-        for sent in tqdm(tagged_sent):
-            if not sent:
-                continue
-            s, t = zip(*sent)
-            word_set.update(s)
-            for i in range(len(sent)):
-                X.append(feature_dict(s, i))
-                y.append((t[i]))
-        self.word_set = word_set
-        return X, y
-        
-        
-        
-        
-        
-        
-        
-        
+        return not(w in self.words)
